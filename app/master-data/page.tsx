7df82f64cf1c7cded4bf1769a3e975ae1b4828cc
Arpit Yadav Personal <arpityadav1804@gmail.com>
2026-03-30 12:19:14 +0000

    Removed Border in MAster Data Management

--- app/master-data/page.tsx
+++ app/master-data/page.tsx
@@ -84,7 +84,7 @@ export default function MasterDataPage() {
           />
         </div>
         <div className="border-b border-slate-200 dark:border-slate-700 mb-4" />
-        <div>
+        <div className="bg-white dark:bg-slate-800 rounded-lg shadow-sm p-4">
           <UnifiedMasterDataV2 userId={user.id} userRole={userRole} selectedGroupId={selectedGroupId} hideCardWrapper={true} />
         </div>
       </div>
